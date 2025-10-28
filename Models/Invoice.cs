5ee439e59ea94c51f1159a0013c29e03739c4b0c
Bruno Bechara <Brunosbechara@gmail.com>
2025-10-28 20:47:45 -0300

    feat: add Invoice model

---
+++ Models/Invoice.cs
@@ -0,0 +1,15 @@
+﻿namespace OrderEvent.Models;
+
+public class Invoice
+{
+    public int Id { get; set; }
+
+    public int OrderId { get; set; }
+    public Order Order { get; set; }
+    public string OrderNumber { get; set; }
+    public string Client { get; set; }
+    public decimal TotalValue { get; set; }
+    public string InvoiceNumber { get; set; }
+    public DateTime IssuedAt { get; set; }
+}
+
