67298b3774be35d2cf318d05deaca514e8889895
Bruno Bechara <Brunosbechara@gmail.com>
2025-10-31 17:55:48 -0300

    feat: add createdAt and updatedAt to category model

--- Models/Category.cs
+++ Models/Category.cs
@@ -6,6 +6,9 @@ public class Category
     public string Name { get; set; }
     public string Description { get; set; }
     public bool Active { get; set; }
-    
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+
     public ICollection<Product> Products { get; set; }
 }
