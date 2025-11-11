02f6c9f4ce72b6c1d6731b4a959f96e60103b4f7
Bruno Bechara <Brunosbechara@gmail.com>
2025-11-10 23:43:22 -0300

    feat: transform OrderNumber from string to guid

---
+++ Models/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderEvent.Models;
+
+public class ResponseModel<T>
+{
+    public dynamic Dados { get; set; }
+    public string Mensagem { get; set; }
+    public bool Status { get; set; }
+}
+
